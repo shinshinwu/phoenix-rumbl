d965c77f73506b6fe291a2167efb5aed1e788637
Anna Wu <shinshinwu@gmail.com>
2018-12-26 13:52:01 -0800

    add heroku specific config

--- config/prod.exs
+++ config/prod.exs
@@ -11,11 +11,19 @@ use Mix.Config
 # before starting your production server.
 config :rumbl, RumblWeb.Endpoint,
   http: [:inet6, port: System.get_env("PORT") || 4000],
-  url: [host: "example.com", port: 80],
-  cache_static_manifest: "priv/static/cache_manifest.json"
+  url: [scheme: "https", host: "rumbl-on-phoenix.herokuapp.com", port: 443],
+  force_ssl: [rewrite_on: [:x_forwarded_proto]],
+  cache_static_manifest: "priv/static/cache_manifest.json",
+  secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE")
 
 # Do not print debug messages in production
 config :logger, level: :info
+
+# Database settings
+config :rumbl, Rumbl.Repo,
+  url: System.get_env("DATABASE_URL"),
+  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
+  ssl: true
 
 # ## SSL Support
 #
@@ -68,4 +76,6 @@ config :logger, level: :info
 
 # Finally import the config/prod.secret.exs which should be versioned
 # separately.
-import_config "prod.secret.exs"
+
+# Commenting this out since secret configs are loaded through heroku
+# import_config "prod.secret.exs"
